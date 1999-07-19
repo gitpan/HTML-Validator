2cde347fab6f21836b89ed83066143ee7bf4fc91
Sami Itkonen <si@iki.fi>
1999-07-19 15:52:30 +0000

    Import of SAIT/HTML-Validator-0.10 from CPAN.
    gitpan-cpan-distribution: HTML-Validator
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         SAIT/HTML-Validator-0.10.tar.gz
    gitpan-cpan-author:       SAIT
    gitpan-cpan-maturity:     released

--- Validator.pm
+++
@@ -1,7 +0,0 @@
-package HTML::Validator;
-$VERSION = 1.0;
-=pod
-This is a dummy HTML::Validator placeholder which is supposed to be
-replaced by the real one by Makefile.PL during the installation phase.
-=cut
-1;  
